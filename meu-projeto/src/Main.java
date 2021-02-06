f86ff2a5176e06958095907957b0c76e20c6422c
felipepborges <felipe_per_borges@hotmail.com>
2021-02-06 14:49:49 -0300

    Adicionando outra versão para teste

--- meu-projeto/src/Main.java
+++ meu-projeto/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello CWI");
+        System.out.println("Teste da versão atualizada no GitHub");
     }
 }
