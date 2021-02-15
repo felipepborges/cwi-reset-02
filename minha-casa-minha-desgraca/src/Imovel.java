c02b78bc4a77860f334404e5afeb0abd17268a35
felipepborges <felipe_per_borges@hotmail.com>
2021-02-15 15:15:14 -0300

    Adicionando exercicio Minha Casa Minha Desgraça (sem o desafio)

---
+++ minha-casa-minha-desgraca/src/Imovel.java
@@ -0,0 +1,28 @@
+public class Imovel {
+
+    private double valor;
+    private Endereco endereco;
+
+    public Imovel(String logradouro, int numero, String complemento, String bairro, String cidade, UnidadeFederativa estado, double valor) {
+       endereco = new Endereco(logradouro, numero, complemento, bairro, cidade, estado);
+        this.valor = valor;
+    }
+    public String apresentacao(){
+        return  "Imóvel localizado no seguinte endereço: \n " +
+                "Logradouro: " + endereco.getLogradouro() +
+                "\nNúmero: " + endereco.getNumero() +
+                "\nComplemento: " + endereco.getComplemento() +
+                "\nBairro: " + endereco.getBairro() +
+                "\nCidade: " + endereco.getCidade() +
+                "\nEstado: " + endereco.getEstado().getNome() +
+                "\nValor do imóvel: R$ " + this.valor + "\n";
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+}
---
+++ minha-casa-minha-desgraca/src/PropostaFinanciamento.java
@@ -0,0 +1,58 @@
+public class PropostaFinanciamento {
+    /*
+     * Valida a proposta de financiamento e apresenta o resultado para o cliente.
+     *
+     * A proposta de financiamento é aceita somente se o salário do beneficiário multiplicado pelo prazo de pagamento
+     *   for equivalente a pelo menos 50% do valor do imóvel.
+     *
+     *   Esta regra possui duas exceções: se o imóvel se localiza no estado SP ou RJ,
+     *      o salário multiplicado pelo prazo deve ser equivalente a 65% e 60% do valor imóvel (respectivamente).
+     */
+
+    private Beneficiario beneficiario;
+    private Imovel imovelEscolhido;
+    private int mesesParaPagamento;
+
+    public PropostaFinanciamento(Beneficiario beneficiario, Imovel imovelEscolhido, int mesesParaPagamento) {
+        this.beneficiario = beneficiario;
+        this.imovelEscolhido = imovelEscolhido;
+        this.mesesParaPagamento = mesesParaPagamento;
+    }
+
+    public void validarProposta() {
+
+        double relacaoValorImovelSalario = (imovelEscolhido.getValor()/(beneficiario.getSalario()*mesesParaPagamento));
+
+        if (imovelEscolhido.getEndereco().getEstado() == UnidadeFederativa.SAO_PAULO && relacaoValorImovelSalario <= 0.65){
+            imprimirPropostaAprovada();
+
+        } else if(imovelEscolhido.getEndereco().getEstado() == UnidadeFederativa.RIO_DE_JANEIRO && relacaoValorImovelSalario <= 0.6) {
+            imprimirPropostaAprovada();
+
+        }else if(imovelEscolhido.getEndereco().getEstado() != UnidadeFederativa.RIO_DE_JANEIRO &&
+                imovelEscolhido.getEndereco().getEstado() != UnidadeFederativa.SAO_PAULO &&
+                relacaoValorImovelSalario <= 0.5){
+            imprimirPropostaAprovada();
+
+        }else{
+            imprimirPropostaNegada();
+        }
+
+
+    }
+    private void imprimirPropostaAprovada(){
+        System.out.println("Beneficiário: " + beneficiario.getNome());
+        System.out.println("Imóvel escolhido: " + imovelEscolhido.apresentacao());
+        System.out.println("Prazo para pagamento: " + mesesParaPagamento);
+        System.out.println("Parabéns! Sua proposta foi aceita!");
+    }
+
+    private void imprimirPropostaNegada(){
+        System.out.println("Beneficiário: " + beneficiario.getNome());
+        System.out.println("Imóvel escolhido: " + imovelEscolhido.apresentacao());
+        System.out.println("Prazo para pagamento: " + mesesParaPagamento);
+        System.out.println("Não foi dessa vez! Proposta negada!");
+    }
+
+
+}
